a55971870fa58ca8e4822effde7e22ae801cf801
Ilya <doggi.v@mail.ru>
2022-12-07 12:19:49 +0600

    task_23 complete

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -6,3 +6,15 @@
 // 5 -> 1, 8, 27, 64, 125
 
 Console.Clear();
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
